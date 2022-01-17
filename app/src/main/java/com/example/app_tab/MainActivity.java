7400a9bf0c3559cb1de68d043c1df9e71bb8e745
MichelFaye <94284260+MichelFaye@users.noreply.github.com>
2022-01-17 16:36:48 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/app_tab/MainActivity.java
+++ app/src/main/java/com/example/app_tab/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler
+import android.os.Handler;
 
 import com.example.myapp.R;
 
